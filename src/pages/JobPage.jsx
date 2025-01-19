f65a7315415614627c11a28d986279f7b74334bc
RBLucky <nkosi.s.lucky@gmail.com>
2025-01-19 22:29:51 +0200

    added addJob form

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,4 +1,5 @@
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, Link } from "react-router-dom";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 
 const JobPage = () => {
   const { id } = useParams();
@@ -8,12 +9,12 @@ const JobPage = () => {
     <>
         <section>
       <div className="container m-auto py-6 px-6">
-        <a
-          href="/jobs.html"
+        <Link
+          to="/jobs"
           className="text-indigo-500 hover:text-indigo-600 flex items-center"
         >
-          <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
-        </a>
+          <FaArrowLeft className="mb-0.5 mr-2" /> Back to Job Listings
+        </Link>
       </div>
     </section>
 
@@ -31,9 +32,7 @@ const JobPage = () => {
               <div
                 className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
-                <i
-                  className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                ></i>
+                <FaMapMarker className="text-orange-700 mr-1 mt-0.5" />
                 <p className="text-orange-700">{job.location}</p>
               </div>
             </div>
@@ -75,16 +74,16 @@ const JobPage = () => {
 
               <h3 className="text-xl">Contact Phone:</h3>
 
-              <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+              <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
             </div>
 
             {/* <!-- Manage --> */}
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <a
-                href="/add-job.html"
+              <Link
+                to={`/jobs/edit/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</a
+                >Edit Job</Link
               >
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
