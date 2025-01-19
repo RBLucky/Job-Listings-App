29a8fba7f5d44b30960146fd63155297bd9cdf79
RBLucky <nkosi.s.lucky@gmail.com>
2025-01-19 23:32:01 +0200

    added functions and props for deleting

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,11 @@ const App = () => {
 
     return;
   };
+
+  //Delete Existing Job
+  const deleteJob = async (id) => {
+    console.log('delete', id);
+  }
   
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -36,7 +41,7 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="/jobs/:id" element={<JobPage deleteJob={ deleteJob } />} loader={jobLoader} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,20 @@
-import { useParams, useLoaderData, Link } from "react-router-dom";
+import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm('THis action is irreversible! Are you sure you want to delete this listing?')
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate('/jobs');
+  }
 
   return (
     <>
@@ -86,6 +97,7 @@ const JobPage = () => {
                 >Edit Job</Link
               >
               <button
+                onClick={ () => onDeleteClick(job.id) }
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
